738cc35d26d2853f92e72eb4cec8cbca03d53c76
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-14 21:03:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-     "Fizz"
-  elsif num % 5 ==0
+  if num % 3 == 0 && num % 5 == 0
+     return "Fizz"
+  elsif num % 3 == 0
      "Buzz"
-  elsif num % 3 == 0 && num % 5 == 0
+  elsif num % 5 == 0
      "FizzBuzz"
   end
 end
